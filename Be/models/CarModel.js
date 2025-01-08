2ff2195aa519fc0266ceccd902db069aac95a1b1
TrungLe2003 <trungle.kqt@gmail.com>
2025-01-08 21:27:53 +0700

    add carModel

---
+++ Be/models/CarModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const carSchema = new mongoose.Schema({
+  carName: {
+    type: String,
+    required: true,
+  },
+  carImg: String,
+  carPrice: {
+    type: Number,
+    required: true,
+  },
+  color: String,
+  version: String,
+  ODO: String, //odograph: máy ghi quãng đường của ô tô
+  year: Number,
+  origin: String, //xuất xứ
+  gearBox: { type: String, enum: ["Tự động", "Số sàn"], default: "Tự động" }, //Hộp số
+  driveSystem: String, //Hệ dẫn động,
+  torque: String, //Momen xoắn,
+  engine: String, //Động cơ
+  horsePower: String, //mã lực
+  power: String, //năng lượng
+  //   idProvider: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: '',
+  //   },
+});
+
+const CarModel = mongoose.model("cars", carSchema);
+
+export default CarModel;
