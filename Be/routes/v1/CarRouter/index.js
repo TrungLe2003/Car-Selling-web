b4a40b858925b4d3eb4848407ea2dc7d243e2da9
PhuongNguyen61 <phuongnguyen.612003@gmail.com>
2025-01-12 12:58:07 +0700

    Delete Be/routes/v1/CarRouter directory

--- Be/routes/v1/CarRouter/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import CarController from "../../../controllers/carController.js";
-const CarRouter = Router();
-
-CarRouter.post("/create-car", CarController.createCar);
-CarRouter.get("/car/:id", CarController.getCar);
-CarRouter.get("/car", CarController.getListCar);
-
-export default CarRouter;
