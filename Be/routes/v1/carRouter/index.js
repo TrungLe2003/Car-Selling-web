2f918dba989976b2235b222dfcc465561634a807
TrungLe2003 <trungle.kqt@gmail.com>
2025-01-14 16:06:53 +0700

    chỉnh lại createCar, thêm chi tiết xe fe

--- Be/routes/v1/carRouter/index.js
+++ Be/routes/v1/carRouter/index.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import CarController from "../../../controllers/carController.js";
+import UserMiddleware from "../../../middlewares/userMiddleware.js";
 const CarRouter = Router();
 
-CarRouter.post("/create-car", CarController.createCar);
-CarRouter.get("/car/:id", CarController.getCar);
+//Thêm xe mới (đã là provider - role: 'PROVIDER')
+CarRouter.post(
+  "/create-car",
+  UserMiddleware.checkRoleProvider,
+  CarController.createCar
+);
+CarRouter.get("/:id", CarController.getCarById);
 CarRouter.get("/car", CarController.getListCar);
 
 export default CarRouter;
--- Be/routes/v1/index.js
+++ Be/routes/v1/index.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import UserRouter from "./userRouter/index.js";
-import CarRouter from "./CarRouter/index.js";
+import CarRouter from "./carRouter/index.js";
 const V1Router = Router();
 
 V1Router.use("/users", UserRouter);
