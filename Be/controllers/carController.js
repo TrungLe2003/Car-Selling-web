d23320267cc8f90c32e21f13dea2a18a6af7f061
TrungLe2003 <trungle.kqt@gmail.com>
2025-02-11 18:32:19 +0700

    sửa lại api cho a Duy

--- Be/controllers/carController.js
+++ Be/controllers/carController.js
@@ -222,59 +222,89 @@ const CarController = {
     } catch (error) {
       res.status(500).send({
         message: error.message,
-        data: null
-      })
+        data: null,
+      });
     }
   },
 
   ////api sửa thông tin của xe
   updatedCar: [
-    uploadImgCar,
+    // uploadImgCar,
     async (req, res) => {
       try {
         const { idCar } = req.params;
-        ////Tải lên Cloudinary và lấy URL
-        const files = req.files;
-        if (!files || files.length === 0) {
-          return res
-            .status(400)
-            .send({ message: "Chưa có file ảnh nào được tải lên" });
-        }
+        // ////Tải lên Cloudinary và lấy URL
+        // const files = req.files;
+        // if (!files || files.length === 0) {
+        //   return res
+        //     .status(400)
+        //     .send({ message: "Chưa có file ảnh nào được tải lên" });
+        // }
 
         ////Duyệt qua tất cả các ảnh và tải lên Cloudinary
-        const imageUrls = await Promise.all(files.map(uploadToCloudinary));
-        const { carName, carPrice, carImg, color, version, ODO, year, origin, gearBox, driveSystem, torque, engine, horsePower, power, brand, describe, state, sitChairs } = req.body;
-        const updatedCar = await CarModel.findByIdAndUpdate(idCar, { carName, carPrice, carImg: imageUrls, color, version, ODO, year, origin, gearBox, driveSystem, torque, engine, horsePower, power, brand, describe, state, sitChairs });
+        // const imageUrls = await Promise.all(files.map(uploadToCloudinary));
+        const {
+          carPrice,
+          color,
+          ODO,
+          year,
+          origin,
+          gearBox,
+          driveSystem,
+          torque,
+          engine,
+          horsePower,
+          power,
+          brand,
+          describe,
+          state,
+          sitChairs,
+        } = req.body;
+        const updatedCar = await CarModel.findByIdAndUpdate(idCar, {
+          carPrice,
+          color,
+          ODO,
+          year,
+          origin,
+          gearBox,
+          driveSystem,
+          torque,
+          engine,
+          horsePower,
+          power,
+          brand,
+          describe,
+          state,
+          sitChairs,
+        });
+        //xóa phần img với name ở 2 dòng trên
         res.status(201).send({
-          message: 'Update successful!',
-          data: updatedCar
-        })
+          message: "Update successful!",
+          data: updatedCar,
+        });
       } catch (error) {
         res.status(500).send({
           message: error.message,
-          data: null
-        })
+          data: null,
+        });
       }
-
-    }
+    },
   ],
-  /////api xoá xe 
+  /////api xoá xe
   deleteCar: async (req, res) => {
     try {
-      const {idCar} = req.params;
+      const { idCar } = req.params;
       const deleteCar = await CarModel.findByIdAndDelete(idCar);
       res.status(201).send({
-        message: 'Delete successful!',
-        data: deleteCar
-      })
+        message: "Delete successful!",
+        data: deleteCar,
+      });
     } catch (error) {
       res.status(500).send({
         message: error.message,
-      })
+      });
     }
-  }
-    
-
+  },
 };
 
 export default CarController;
