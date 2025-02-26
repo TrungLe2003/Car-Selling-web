dc8d285a492e81f07425d9462f99d026d068ce04
TrungLe2003 <trungle.kqt@gmail.com>
2025-02-27 00:09:54 +0700

    Fix lại socket

--- Fe/src/components/profilePage/addToProfilePage/UserMail.jsx
+++ Fe/src/components/profilePage/addToProfilePage/UserMail.jsx
@@ -11,54 +11,11 @@ import MailSend from "../../../icons/usermail/mailSend";
 //css
 import "./UserMail.css";
 
-// const socket = io("http://localhost:8080"); // Kết nối đến server
-
 const MailPage = () => {
   const [mails, setMails] = useState([]);
   const [selectedMail, setSelectedMail] = useState(null);
-  const [notifications, setNotifications] = useState([]);
   const { userId } = useParams();
   console.log(userId);
-
-  // useEffect(() => {
-  //   if (!userId) return;
-  //   socket.emit("join_room", userId); //tham gia phòng khi vào trang
-  //   socket.on("connect", () => {
-  //     console.log("🔗 Kết nối thành công:", socket.id);
-  //   });
-  //   socket.on("connect_error", (err) => {
-  //     console.error("❌ Lỗi kết nối:", err.message);
-  //   });
-  //   // 3️⃣ Lắng nghe sự kiện khi thư được cập nhật
-  //   socket.on("mailStatusChanged", (data) => {
-  //     console.log("📩 Cập nhật mới từ server:", data);
-
-  //     // Thêm thông báo vào danh sách (hiển thị trên UI)
-  //     setNotifications((prev) => [...prev, data]);
-  //     console.log(notifications);
-
-  //     // Cập nhật trạng thái thư trong danh sách
-  //     setMails((prevMails) =>
-  //       prevMails.map((mail) =>
-  //         mail._id === data.mailId ? { ...mail, status: data.status } : mail
-  //       )
-  //     );
-
-  //     // Ẩn thông báo sau 5 giây
-  //     setTimeout(() => {
-  //       setNotifications((prev) =>
-  //         prev.filter((n) => n.mailId !== data.mailId)
-  //       );
-  //     }, 8000);
-  //   });
-  //   socket.on("mailStatusChanged", (data) => {
-  //     console.log("📩 Cập nhật mới từ server:", data);
-  //   });
-
-  //   return () => {
-  //     socket.off("mailStatusChanged"); // Cleanup listener khi rời trang
-  //   };
-  // }, [userId]);
   useEffect(() => {
     const fetchUserMailData = async () => {
       try {
